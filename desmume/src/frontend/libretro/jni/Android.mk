dc96e7fa262784bf42483bb0e7e1d03e216f6cda
Filippo Scognamiglio <flscogna@gmail.com>
2026-04-19 19:02:21 +0200

    Add support for 16KB pages on Android.

--- desmume/src/frontend/libretro/jni/Android.mk
+++ desmume/src/frontend/libretro/jni/Android.mk
@@ -30,6 +30,6 @@ LOCAL_MODULE       := retro
 LOCAL_SRC_FILES    := $(SOURCES_CXX) $(SOURCES_C)
 LOCAL_CXXFLAGS     := $(COREFLAGS) -std=gnu++11
 LOCAL_CFLAGS       := $(COREFLAGS)
-LOCAL_LDFLAGS      := -Wl,-version-script=$(LIBRETRO_DIR)/link.T
+LOCAL_LDFLAGS      := -Wl,-version-script=$(LIBRETRO_DIR)/link.T,-z,max-page-size=16384
 LOCAL_CPP_FEATURES := exceptions
 include $(BUILD_SHARED_LIBRARY)
